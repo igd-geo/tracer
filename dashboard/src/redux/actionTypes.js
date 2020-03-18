23153e3f5cf9d63a9d622686bbeb73a5f31999b8
Dennis Appel <dennis.appel@igd.fraunhofer.de>
2020-03-18 21:50:59 +0100

    Remove dashboard and .idea files

--- dashboard/src/redux/actionTypes.js
+++
@@ -1,2 +0,0 @@
-export const SET_ACTIVE_NODE = 'SET_ACTIVE_NODE';
-export const SET_GRAPH = 'SET_GRAPH';
--- dashboard/src/redux/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import app from './app';
-import metadata from './metadata';
-import graph from './graph';
-
-export default combineReducers({
-  app, graph, metadata,
-});
